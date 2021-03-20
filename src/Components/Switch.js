a49715334078adced18e05025f16c655f1749b3d
May-man <mayman@beyond-creation.net>
2021-03-20 23:58:19 +0200

    remove unsed code

--- src/Components/Switch.js
+++ src/Components/Switch.js
@@ -2,19 +2,15 @@ import React, { useState } from "react";
 import useAxios from "axios-hooks";
 import CastTabs from "./CastTabs";
 import CurrentCast from "./CurrentCast";
-import cloud from "../Imgs/cloud.png"
+import cloud from "../Imgs/cloud.png";
+import sadCloud from "../Imgs/sadCloud.png";
+
 function Switch() {
   const [active, setActive] = useState(true);
-
-
   const apiKey = "a177f8481c31fa96c3f95ad4f4f84610";
-  const lat = "30.8220";
-  const lon = "37.7510";
+  let lat = "30.0778";
+  let lon = "31.2852";
   const proxy = "https://cors-anywhere.herokuapp.com/";
-  const [{ geoData, geoloading, geoerror }, refetchGeo] = useAxios(
-    `${proxy}http://ip-api.com/json`
-  );
-  console.log(geoData)
   let url = "";
   let urlFhern = `${proxy}https://api.darksky.net/forecast/${apiKey}/${lat},${lon}`;
   let urlCel = `${proxy}https://api.darksky.net/forecast/${apiKey}/${lat},${lon}?units=si`;
@@ -24,16 +20,23 @@ function Switch() {
     url = urlFhern;
   }
   const [{ data, loading, error }, refetch] = useAxios(url);
-  function handleClick(){
+  function handleClick() {
     setActive(!active);
-    refetch(url)
+    refetch(url);
   }
-
-    
-  if (loading) return <p className="loading">
-    <img src={cloud} alt="cloud"/>
-  </p>;
-  if (error) return <p className="error">Error!</p>;
+  if (loading)
+    return (
+      <p className="loading">
+        <img src={cloud} alt="cloud" />
+      </p>
+    );
+  if (error)
+    return (
+      <p className="error">
+        <img src={sadCloud} alt="sad cloud" />
+        Api call failed
+      </p>
+    );
 
   return (
     <>
